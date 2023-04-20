1af17d11bb91a4f71ba254b2a5811c22577563f8
XavierPelle <Xavierp34970@gmail.com>
2023-04-20 17:01:53 +0200

    view + nom_vernaculaire

--- src/Entity/Famille.php
+++ src/Entity/Famille.php
@@ -51,4 +51,5 @@ class Famille extends Model {
 
         return $this;
     }
+
 }
--- views/especes/create.php
+++ views/especes/create.php
@@ -10,6 +10,10 @@
                 <input type="text" class="form-control" id="nomScientifique" name="nomScientifique" value="<?php echo (isset($modifesp)) ? $modifesp->nomScientifique : '';?>">
             </div>
             <div class="mb-3">
+                <label for="nomVernaculaire" class="form-label">Nom vernaculaire</label>
+                <input type="text" class="form-control" id="nomVernaculaire" name="nomVernaculaire" value="">
+            </div>
+            <div class="mb-3">
                 <label for="altitude" class="form-label">Altitude</label>
                 <input type="number" class="form-control" id="altitude" name="altitude" value="<?php echo (isset($modifesp)) ? $modifesp->altitude : '';?>">
             </div>
@@ -21,7 +25,7 @@
                 <label for="idStatut" class="form-label">Statut</label>
                 <select class="form-select" id="idStatut" name="idStatut">
                 <?php foreach ($especess as $esps):?>
-                <option value="<?php echo $esps->id; ?>"<?php echo ($modifesp->id === $esps->id)? 'selected':''?>><?php echo $esps->codeStatut;?> - <?php echo $esps->statut;?></option>
+                    <option value="<?php echo $esps->id; ?>"<?php echo (isset($modifesp) && $modifesp->idStatut === $esps->id)? 'selected':''?>><?php echo $esps->codeStatut;?> - <?php echo $esps->statut;?></option>
                 <?php endforeach ?>
                 </select>
             </div>
@@ -29,7 +33,7 @@
                 <label for="idFamille" class="form-label">Famille</label>
                 <select class="form-select" id="idFamille" name="idFamille">
                 <?php foreach ($especes as $esp):?>      
-                <option value="<?php echo $esp->id; ?>"<?php echo ($modifesp->id === $esp->id)? 'selected':''?>><?php echo $esp->nomFamille;?></option>
+                    <option value="<?php echo $esp->id; ?>"<?php echo (isset($modifesp) && $modifesp->idFamille === $esp->id)? 'selected':''?>><?php echo $esp->nomFamille;?></option>
                 <?php endforeach ?>
                 </select>
             </div>
